7fe4b9911b141426ac6d02fc4ee1733a057ccc33
aquine-kujaruk <37378346+aquine-kujaruk@users.noreply.github.com>
2023-06-04 15:56:40 +0200

    Initial configs

--- bin/app.ts
+++ bin/app.ts
@@ -1,21 +1,23 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+import { App, Tags } from 'aws-cdk-lib';
 import { AppStack } from '../lib/app-stack';
+import { config } from 'dotenv';
 
-const app = new cdk.App();
+config();
+
+const app = new App();
+
+const appName: string = process.env.APP_NAME || 'cdk-app';
+console.log('appName: ', appName);
+const environment: string = process.env.ENVIRONMENT || 'dev';
+console.log('environment: ', environment);
+const region: string = process.env.AWS_REGION || 'us-east-1';
+console.log('region: ', region);
+
+Tags.of(app).add('App', appName);
+Tags.of(app).add('Environment', environment);
+
 new AppStack(app, 'AppStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+    env: { region },
 });
